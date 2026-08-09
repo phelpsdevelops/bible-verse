7e2efb77ae7749b882e65106bb67661c2db8bae6
R. Phelps <raquanp78@icloud.com>
2026-08-09 14:53:36 -0400

    bible verse of the day fetch api call

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
 
+getVerseOfTheDay()
+
+ function getVerseOfTheDay(){
+  
+    
+    const url=`https://quotes.rest/bible/vod.json?api_key=${API_KEY}`
+
+        fetch(url)
+        .then(res => res.json())
+        .then(data=>{
+            console.log(data)
+            
+
+        })
+        .catch(err =>{
+            console.log(`error ${err}`)
+        });
+
+}
